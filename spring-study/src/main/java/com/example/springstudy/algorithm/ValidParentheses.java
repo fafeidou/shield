f2eb62254fb91f1e58a47f19be0f9e3e386e0928
batman <943104990@qq.com>
2023-12-28 16:48:10 +0800

    feat : ValidParentheses

---
+++ spring-study/src/main/java/com/example/springstudy/algorithm/ValidParentheses.java
@@ -0,0 +1,37 @@
+package com.example.springstudy.algorithm;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        String s ="(){}}{";
+        System.out.println(isValid(s));
+    }
+
+    private static final Map<Character, Character> map = new HashMap<Character, Character>() {{
+        put('{', '}');
+        put('[', ']');
+        put('(', ')');
+        put('?', '?');
+    }};
+
+    public static boolean isValid(String s) {
+        if (s.length() > 0 && !map.containsKey(s.charAt(0))) {
+            return false;
+        }
+        LinkedList<Character> stack = new LinkedList<Character>() {{
+            add('?');
+        }};
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.addLast(c);
+            } else if (map.get(stack.removeLast()) != c) {
+                return false;
+            }
+        }
+        return stack.size() == 1;
+    }
+}
