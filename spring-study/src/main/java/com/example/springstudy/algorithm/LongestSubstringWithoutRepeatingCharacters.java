ce1b17c39109a7fa0fc22afeb2090ce8bfcec045
batman <943104990@qq.com>
2023-12-29 16:53:02 +0800

    LongestSubstringWithoutRepeatingCharacters

---
+++ spring-study/src/main/java/com/example/springstudy/algorithm/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,31 @@
+package com.example.springstudy.algorithm;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        int max = 0;
+        int left = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        int i = 0;
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            }
+            map.put(c, i);
+            max = Math.max(max, i - left + 1);
+            i++;
+        }
+        return max;
+    }
+}
