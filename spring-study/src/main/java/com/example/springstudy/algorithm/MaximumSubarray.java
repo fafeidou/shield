a9c8f4a1ea67a7e6c838c901dfa494da16541b73
batman <943104990@qq.com>
2023-12-30 10:34:41 +0800

    MaximumSubarray

---
+++ spring-study/src/main/java/com/example/springstudy/algorithm/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package com.example.springstudy.algorithm;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        maxSubArray(array);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+    public static int maxSubArray2(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
