94590d80522c9ccc7d04930d7b9a6bb0abba62ba
batman <943104990@qq.com>
2024-08-28 15:20:22 +0800

    feat : method area test

---
+++ jvm-study/src/main/java/com/shield/MethodAreaTest.java
@@ -0,0 +1,8 @@
+package com.shield;
+
+public class MethodAreaTest {
+    public static int count = 1;
+    public static final int number = 2;
+    public static void main(String[] args) {
+    }
+}
